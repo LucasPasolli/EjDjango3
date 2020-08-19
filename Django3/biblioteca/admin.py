67afd4f0d2e92ee9425867782a38e881ff55f4b1
LucasPasolli <lippasolli@hotmail.com>
2020-08-19 20:11:13 -0300

    Mas Inlines

--- Django3/biblioteca/admin.py
+++ Django3/biblioteca/admin.py
@@ -4,6 +4,9 @@ from .models import *
 class MaterialInline(admin.TabularInline):
     model = Material
     fields = ['tipoMaterial','autor','titulo','año','status']
+    extra = 0
+
+    
 
 class PersonaInline(admin.TabularInline):
     model = Persona
@@ -11,6 +14,21 @@ class PersonaInline(admin.TabularInline):
 
 class PrestamoAdmin(admin.ModelAdmin):
     inlines = [MaterialInline, PersonaInline]
+   
+class LibroAdmin(admin.ModelAdmin): 
+    list_display = ['tipoMaterial','codigo','autor','titulo','año','status','editorial',]
+   
+class RevistaAdmin(admin.ModelAdmin): 
+    list_display = ['tipoMaterial','codigo','autor','titulo','año','status',]
+
+class AlumnoAdmin(admin.ModelAdmin): 
+    list_display = ['matricula','tipoPersona','nombre','apellido','correo','telefono','numLibros', 'adeudo',]
+
+class ProfesorAdmin(admin.ModelAdmin): 
+    list_display = ['numEmpleado', 'tipoPersona', 'nombre', 'apellido', 'correo', 'telefono', 'numLibros', 'adeudo',]
+   
+
+
 
 # Register your models here.
 
--- Django3/biblioteca/models.py
+++ Django3/biblioteca/models.py
@@ -58,23 +58,23 @@ class Libro(Material):
     editorial = models.CharField(max_length=50)
 
     def __str__(self):
-        return str(Material.titulo) 
+        return str(self.titulo) 
 
 class Revista(Material):
 
     def __str__(self):
-        return str(Material.titulo) 
+        return str(self.titulo) 
 
 class Profesor(Persona):
     
     numEmpleado = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return str(Persona.nombre)
+        return str(self.nombre)
 
 class Alumno(Persona):
     
     matricula = models.AutoField(primary_key=True)
 
     def __str__(self):
-        return str(Persona.nombre)
+        return str(self.nombre)
