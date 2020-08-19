9dd7d8859fd604cf11c43f9e868496019e1abc94
LucasPasolli <lippasolli@hotmail.com>
2020-08-19 19:35:18 -0300

    Adicion de Inlines

--- Django3/biblioteca/admin.py
+++ Django3/biblioteca/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from .models import *
 
+class MaterialInline(admin.TabularInline):
+    model = Material
+    fields = ['tipoMaterial','autor','titulo','año','status']
+
+class PersonaInline(admin.TabularInline):
+    model = Persona
+    fields = ['tipoPersona','nombre','apellido','correo','telefono','numLibros','adeudo']
+
+class PrestamoAdmin(admin.ModelAdmin):
+    inlines = [MaterialInline, PersonaInline]
+
 # Register your models here.
 
-admin.site.register(Prestamo)
+admin.site.register(Prestamo, PrestamoAdmin)
 admin.site.register(Persona)
 admin.site.register(Profesor)
 admin.site.register(Alumno)
